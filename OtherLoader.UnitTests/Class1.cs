8879bf41906a8be9c61ba670103690f1e6abebb0
Devyn Myers <devyndamonster@hotmail.com>
2022-12-17 18:31:40 -0500

    Added unit test setup

---
+++ OtherLoader.UnitTests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using OtherLoader.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OtherLoader.UnitTests
+{
+    
+    public class Tests
+    {
+        [Test]
+        public void TestTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestTestA()
+        {
+            var test = new Class1();
+            Assert.IsTrue(test.GetTestStuff());
+        }
+
+    }
+}
