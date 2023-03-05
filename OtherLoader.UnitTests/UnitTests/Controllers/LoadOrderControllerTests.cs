08e394c351e7ab40c70fce24f92e59de027289d1
Devyn Myers <devyndamonster@hotmail.com>
2023-03-05 12:02:23 -0500

    Started on load order tests

---
+++ OtherLoader.UnitTests/UnitTests/Controllers/LoadOrderControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OtherLoader.UnitTests.Controllers
+{
+    public class LoadOrderControllerTests
+    {
+        [TestFixture]
+        public class CanBundleBeginLoading
+        {
+            [Test]
+            public void LoadFirstWontLoadWhileLoadFirstDependancyWaiting()
+            {
+                throw new NotImplementedException();
+            }
+
+            [Test]
+            public void LoadFirstWontLoadWhileLoadFirstDependancyLoading()
+            {
+                throw new NotImplementedException();
+            }
+
+            [Test]
+            public void LoadFirstWillLoadWhileLoadUnorderedDependancyWaiting()
+            {
+                throw new NotImplementedException();
+            }
+
+            //TODO more tests
+        }
+    }
+}
