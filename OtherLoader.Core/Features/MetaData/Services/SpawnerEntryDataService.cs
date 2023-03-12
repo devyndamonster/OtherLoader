65ce7d24348dd083de5eeb3575efb3fb0ef5442a
Devyn Myers <devyndamonster@hotmail.com>
2023-03-12 12:43:24 -0400

    Added page parsing logic for item spawner Ids, still need to give access to FVRObject data

---
+++ OtherLoader.Core/Features/MetaData/Services/SpawnerEntryDataService.cs
@@ -0,0 +1,143 @@
+﻿using OtherLoader.Core.Features.MetaData.Models.Vanilla;
+using OtherLoader.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OtherLoader.Core.Features.MetaData.Services
+{
+    public class SpawnerEntryDataService : ISpawnerEntryDataService
+    {
+        private readonly IEnumerable<ItemCategory> _displayedItemCategories = new[]
+        {
+            ItemCategory.MeatFortress,
+            ItemCategory.Magazine,
+            ItemCategory.Cartridge,
+            ItemCategory.Clip,
+            ItemCategory.Speedloader,
+        };
+
+        public SpawnerEntryData ConvertToSpawnerEntryData(ItemSpawnerId spawnerId)
+        {
+            return new SpawnerEntryData
+            {
+                Path = GetSpawnerEntryPathFromSpawnerId(spawnerId)
+            };
+        }
+
+        private string GetSpawnerEntryPathFromSpawnerId(ItemSpawnerId spawnerId)
+        {
+            string path = GetPageForSpawnerId(spawnerId).ToString();
+
+            if (ShouldDisplayMainCategory(spawnerId))
+            {
+                path += "/" + GetTagFromCategory(spawnerId.Category);
+            }
+
+            if (ShouldDisplaySubcategory(spawnerId))
+            {
+                path += "/" + GetTagFromSubcategory(spawnerId.SubCategory);
+            }
+
+            path += "/" + spawnerId.MainObjectId;
+
+            return path;
+        }
+
+        private PageMode GetPageForSpawnerId(ItemSpawnerId spawnerId)
+        {
+            if (ShouldItemBeTaggedAsToolsToy(spawnerId))
+            {
+                return PageMode.ToolsToys;
+            }
+            else if (ShouldItemBeTaggedAsMelee(spawnerId))
+            {
+                return PageMode.Melee;
+            }
+            else if (ShouldItemBeTaggedAsAmmo(spawnerId))
+            {
+                return PageMode.Ammo;
+            }
+            else if (ShouldItemBeTaggedAsAttachment(spawnerId))
+            {
+                return PageMode.Attachments;
+            }
+            else if (ShouldItemBeTaggedAsFirearm(spawnerId))
+            {
+                return PageMode.Firearms;
+            }
+
+            return PageMode.MainMenu;
+        }
+
+        //TODO: spawnerIds should have it's own Core FVRObject instance
+        private bool ShouldItemBeTaggedAsFirearm(ItemSpawnerId spawnerId)
+        {
+            return 
+                spawnerId.MainObject.Category == FVRObject.ObjectCategory.Firearm ||
+                _firearmItemCategories.Contains(spawnerId.Category) ||
+                !Enum.IsDefined(typeof(ItemCategory), spawnerId.Category);
+        }
+
+        private bool ShouldItemBeTaggedAsToolsToy(ItemSpawnerId spawnerId)
+        {
+            return 
+                spawnerId.MainObject.Category == FVRObject.ObjectCategory.Explosive ||
+                spawnerId.MainObject.Category == FVRObject.ObjectCategory.Thrown ||
+                spawnerId.SubCategory == SubCategory.Grenade ||
+                spawnerId.SubCategory == SubCategory.RemoteExplosives ||
+                spawnerId.Category == ItemCategory.Misc;
+        }
+
+        private bool ShouldItemBeTaggedAsMelee(ItemSpawnerId spawnerId)
+        {
+            return 
+                spawnerId.MainObject.Category == FVRObject.ObjectCategory.MeleeWeapon ||
+                spawnerId.Category == ItemCategory.Melee;
+        }
+
+        private bool ShouldItemBeTaggedAsAmmo(ItemSpawnerId spawnerId)
+        {
+            return 
+                spawnerId.Category == ItemCategory.Magazine ||
+                spawnerId.Category == ItemCategory.Clip ||
+                spawnerId.Category == ItemCategory.Speedloader ||
+                spawnerId.Category == ItemCategory.Cartridge;
+        }
+
+        private bool ShouldItemBeTaggedAsAttachment(ItemSpawnerId spawnerId)
+        {
+            return spawnerId.MainObject.Category == FVRObject.ObjectCategory.Attachment;
+        }
+
+
+        private bool ShouldDisplayMainCategory(ItemSpawnerId spawnerId)
+        {
+            var isDisplayableVanillaCategory = _displayedItemCategories.Contains(spawnerId.Category);
+
+            var isModdedMainCategory = !Enum.IsDefined(typeof(ItemCategory), spawnerId.Category);
+
+            return isDisplayableVanillaCategory || isModdedMainCategory;
+        }
+
+        private bool ShouldDisplaySubcategory(ItemSpawnerId spawnerId)
+        {
+            return spawnerId.SubCategory != SubCategory.None;
+        }
+
+        private string GetTagFromCategory(ItemCategory category)
+        {
+            if (Enum.IsDefined(typeof(ItemCategory), category)) return category.ToString();
+
+            return "ModdedCategory_" + category.ToString();
+        }
+
+        private string GetTagFromSubcategory(SubCategory subcategory)
+        {
+            if (Enum.IsDefined(typeof(SubCategory), subcategory)) return subcategory.ToString();
+
+            return "ModdedSubcategory_" + subcategory.ToString();
+        }
+    }
+}
---
+++ OtherLoader.UnitTests/UnitTests/Services/SpawnerEntryDataServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OtherLoader.Core.Features.MetaData.Models.Vanilla;
+using OtherLoader.Core.Features.MetaData.Services;
+
+namespace OtherLoader.UnitTests.Services
+{
+    public class SpawnerEntryDataServiceTests
+    {
+        [TestFixture]
+        public class ConvertToSpawnerEntryData
+        {
+            [TestCase("G18", ItemCategory.Pistol, SubCategory.MachinePistol, "Firearms/MachinePistol/G18")]
+            [TestCase("MF_Medical180", ItemCategory.MeatFortress, SubCategory.MF_Medic, "Firearms/MeatFortress/MF_Medic/MF_Medical180")]
+            [TestCase("38TroundClip", ItemCategory.Clip, SubCategory.None, "Ammo/Clip/38TroundClip")]
+            [TestCase("SpeedloaderR8", ItemCategory.Speedloader, SubCategory.None, "Ammo/Speedloader/SpeedloaderR8")]
+            [TestCase("10x27mmPulsed", ItemCategory.Cartridge, SubCategory.None, "Ammo/Cartridge/10x27mmPulsed")]
+            [TestCase("MagazineFiveSeven30rnd", ItemCategory.Magazine, SubCategory.None, "Ammo/Magazine/MagazineFiveSeven30rnd")]
+            [TestCase("CustomCategoryItem", (ItemCategory)1234, (SubCategory)5678, "Firearms/ModdedCategory_1234/ModdedSubcategory_5678/CustomCategoryItem")]
+            public void ItWillCreateCorrectPath(string mainObjectId, ItemCategory category, SubCategory subCategory, string expectedPath)
+            {
+                var spawnerId = new ItemSpawnerId
+                {
+                    ItemId = "ItemId",
+                    MainObjectId = mainObjectId,
+                    Category = category,
+                    SubCategory = subCategory
+                };
+
+                var service = new SpawnerEntryDataService();
+
+                var spawnerEntry = service.ConvertToSpawnerEntryData(spawnerId);
+
+                spawnerEntry.Path.Should().Be(expectedPath);
+            }
+
+        }
+    }
+}
